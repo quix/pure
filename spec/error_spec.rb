df833451b78961a8a84e42cee1f4f3ba69c3dd7e
quix <quixoticsycophant@gmail.com>
2009-04-05 20:43:42 -0400

    group examples in error_spec

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -1,39 +1,40 @@
 require File.dirname(__FILE__) + "/../spec/common"
 
-describe "two defs on the same line" do
-  it "should raise error" do
-    lambda {
-      Module.new {
-        include Pure
-        def x ; end ; def y ; end
-      }
-    }.should raise_error(Pure::ParseError)
-  end
-end
-
-describe "`fun'" do
-  describe "given hash of size != 1" do
+describe "error checking:" do
+  describe "two defs on the same line" do
     it "should raise error" do
       lambda {
         Module.new {
           include Pure
-          fun :x => 1, :y => 2 do
-          end
+          def x ; end ; def y ; end
         }
-      }.should raise_error(ArgumentError)
+      }.should raise_error(Pure::ParseError)
     end
   end
 
-  describe "given more than 1 argument" do
-    it "should raise error" do
-      lambda {
-        Module.new {
-          include Pure
-          fun :x, :y do
-          end
-        }
-      }.should raise_error(ArgumentError)
+  describe "`fun'" do
+    describe "given hash of size != 1" do
+      it "should raise error" do
+        lambda {
+          Module.new {
+            include Pure
+            fun :x => 1, :y => 2 do
+            end
+          }
+        }.should raise_error(ArgumentError)
+      end
+    end
+
+    describe "given more than 1 argument" do
+      it "should raise error" do
+        lambda {
+          Module.new {
+            include Pure
+            fun :x, :y do
+            end
+          }
+        }.should raise_error(ArgumentError)
+      end
     end
   end
 end
-
