7777aab19ea9ad9a5bd279f0db3f1187300186e5
quix <quixoticsycophant@gmail.com>
2009-05-17 21:53:18 -0400

    add fun/def mixing examples

--- spec/fun_spec.rb
+++ spec/fun_spec.rb
@@ -46,4 +46,38 @@ describe "`fun' definitions" do
   it "should work with mixed symbols and strings" do
     @mixed.compute(:area, :threads => 4).should == (20 + 5)*(30 + 5)
   end
+
+  it "should work with `def' definitions" do
+    pure do
+      fun :width do
+        33
+      end
+      
+      def height
+        44
+      end
+
+      fun :area => [:width, :height] do |w, h|
+        w*h
+      end
+    end.compute(:area, 3).should == 33*44
+  end
+
+  it "should be lower precedence than `def' definitions" do
+    pure do
+      fun :f do
+        44
+      end
+    end.compute(:f, 10).should == 44
+
+    pure do
+      fun :f do
+        44
+      end
+
+      def f
+        33
+      end
+    end.compute(:f, 10).should == 33
+  end
 end
