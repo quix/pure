6841e283852a02f4661019dca60e621072a9a990
quix <quixoticsycophant@gmail.com>
2009-05-19 02:09:45 -0400

    run spec on ruby_parser and ripper for 1.9

--- spec/common.rb
+++ spec/common.rb
@@ -7,4 +7,31 @@ require 'spec'
 
 include Pure
 
+AVAILABLE_PARSERS = ["ruby_parser"] + (
+  begin
+    require 'ripper'
+    ["ripper"]
+  rescue LoadError
+    []
+  end
+)
 
+module Spec::Example::ExampleGroupMethods
+  alias_method :example__original, :example
+
+  def example(*args, &block)
+    AVAILABLE_PARSERS.each { |parser|
+      describe "(#{parser})" do
+        before :each do
+          Pure.parser = parser
+        end
+        example__original(*args, &block)
+      end
+    }
+  end
+
+  [:it, :specify].each { |method_name|
+    remove_method method_name
+    alias_method method_name, :example
+  }
+end
