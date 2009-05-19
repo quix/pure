5046b2e254708e3ee2e997052f40f85f03c3c8da
quix <quixoticsycophant@gmail.com>
2009-05-18 23:25:27 -0400

    add subsequent_spec

---
+++ spec/subseqent_spec.rb
@@ -0,0 +1,42 @@
+require File.dirname(__FILE__) + "/common"
+
+describe "subsequent `def' definitions" do
+  it "should be accepted" do
+    mod = pure do
+      def f
+        33
+      end
+    end
+
+    mod.compute(:f, 4).should == 33
+
+    mod.module_eval do
+      def g
+        44
+      end
+    end
+
+    mod.compute(:g, 4).should == 44
+  end
+end
+
+describe "subsequent `fun' definitions" do
+  it "should be accepted" do
+    mod = pure do
+      fun :f do
+        33
+      end
+    end
+
+    mod.compute(:f, 4).should == 33
+
+    mod.module_eval do
+      fun :g do
+        44
+      end
+    end
+
+    mod.compute(:g, 4).should == 44
+  end
+end
+
