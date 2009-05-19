3bf66f3114ae9d75b260446530fb38fe1ace0e63
quix <quixoticsycophant@gmail.com>
2009-05-18 22:52:49 -0400

    compute now implemented using generalized build_and_compute

--- lib/pure/pure_private/creator.rb
+++ lib/pure/pure_private/creator.rb
@@ -15,26 +15,38 @@ module Pure
         
         attr_reader :method_database
 
+        def build_and_compute(mod, method_database, root, num_threads, &block)
+          CompTree.build do |driver|
+            mod.ancestors.each { |ancestor|
+              if defs = method_database[ancestor]
+                defs.each_pair { |method_name, spec|
+                  existing_node = driver.nodes[method_name]
+                  if existing_node.nil? or existing_node.function.nil?
+                    node = driver.define(method_name, *spec[:args])
+                    node.function = yield method_name, spec
+                  end
+                }
+              end
+            }
+            driver.compute(root, num_threads)
+          end
+        end
+
+        def instance_compute(mod, root, opts)
+          num_threads = (opts.is_a?(Hash) ? opts[:threads] : opts).to_i
+          instance = Object.new.extend(mod)
+          Creator.build_and_compute(mod, method_database, root, num_threads) {
+            |method_name, spec|
+            lambda { |*args|
+              instance.send(method_name, *args)
+            }
+          }
+        end
+
         def define_compute(mod, method_database)
           singleton_class_of(mod).module_eval do
             define_method :compute do |root, opts|
-              num_threads = (opts.is_a?(Hash) ? opts[:threads] : opts).to_i
-              instance = Class.new { include mod }.new
-              CompTree.build do |driver|
-                mod.ancestors.each { |ancestor|
-                  if defs = method_database[ancestor]
-                    defs.each_pair { |method_name, spec|
-                      existing_node = driver.nodes[method_name]
-                      if existing_node.nil? or existing_node.function.nil?
-                        driver.define(method_name, *spec[:args]) { |*objs|
-                          instance.send(method_name, *objs)
-                        }
-                      end
-                    }
-                  end
-                }
-                driver.compute(root, num_threads)
-              end
+              Creator.instance_compute(mod, root, opts)
             end
           end
         end
