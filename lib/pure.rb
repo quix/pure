5010c7b4dd23f3ba064dc52b00b607d09fc7b35f
quix <quixoticsycophant@gmail.com>
2009-06-21 17:38:40 -0400

    docs

--- lib/pure.rb
+++ lib/pure.rb
@@ -7,11 +7,37 @@ module Pure
 
   module_function
 
+  #
+  # Create a <em>pure module</em>.  The given block is evaluated in
+  # the context of the created module.
+  #
+  # A pure module is a regular Ruby module whose singleton class
+  # includes PurePrivate::SingletonFeatures and whose methods are
+  # specially registered for lexicographical checking.
+  # 
+  # For the purpose of clarity, the methods of a pure module are
+  # called <em>pure functions</em>.
+  #
   def pure(&block)
     PurePrivate::Creator.create_module(&block)
   end
 
   class << self
+    #
+    # call-seq: engine
+    #           engine=(value)
+    #
+    # Query/change the parse engine.  Available engines are:
+    # * :parameters (ruby-1.9.2+ only)
+    # * :ripper (ruby-1.9 only)
+    # * :ruby_parser
+    #
+    # The default engine is tried in that order.
+    #
+    def engine  # for rdoc only
+    end
+    remove_method :engine
+
     [:engine, :engine=].each { |name|
       define_method name, &PurePrivate::Extractor.method(name)
     }
--- lib/pure/pure_private/singleton_features.rb
+++ lib/pure/pure_private/singleton_features.rb
@@ -6,11 +6,56 @@ require 'pure/pure_private/error'
 
 module Pure
   module PurePrivate
+    #
+    # A pure module (returned by Pure.pure) has these singleton
+    # methods.
+    #
     module SingletonFeatures
+      #
+      # call-seq: compute(root, n)
+      #           compute(root, :threads => n)
+      #
+      # Compute a pure function with _n_ threads.
+      #
       def compute(root, opts)
         Driver.create_instance_and_compute(self, root, opts)
       end
       
+      #
+      # call-seq: fun label => [label_a, label_b,...] do |arg_a, arg_b,...|
+      #             ...
+      #           end
+      #
+      # Define a pure function whose name and/or argument names are
+      # not known at compile time.
+      #
+      # The name of the pure function is the value of _label_.  The
+      # names of the function arguments are _label_a_, _label_b_,....
+      # These are assigned to _arg_a_,_arg_b_,... respectively in the
+      # block.
+      #
+      # Example:
+      #
+      #   require 'rubygems'
+      #   require 'pure'
+      #   include Pure
+      #   
+      #   stats = pure do
+      #     files = Dir["*"]
+      #   
+      #     files.each { |file|
+      #       fun file do
+      #         File.size(file)
+      #       end
+      #     }
+      #   
+      #     fun :total_size => files do |*sizes|
+      #       sizes.inject(0) { |acc, size| acc + size }
+      #     end
+      #   end
+      #   
+      #   p stats.compute(:total_size, 3)
+      #
       def fun(*args, &block)
         fun_mod = class << self ; @fun_mod ; end
         node_name, child_names = (
@@ -50,13 +95,13 @@ module Pure
         nil
       end
 
-      def method_added(function_name)
+      def method_added(function_name)  #:nodoc:
         FunctionDatabase[self][function_name] = (
           Extractor.extract(self, function_name, caller).merge(:origin => :def)
         )
       end
 
-      def define_method(*args, &block)
+      def define_method(*args, &block)  #:nodoc:
         raise PurePrivate::NotImplementedError,
         "define_method called (use the `fun' method instead)"
       end
