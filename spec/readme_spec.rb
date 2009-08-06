b682e26e6e6ed327c9bcb9d36f7db934eb6ce8fe
quix <quixoticsycophant@gmail.com>
2009-08-05 23:49:10 -0400

    use Jumpstart.doc_to_spec

--- spec/readme_spec.rb
+++ spec/readme_spec.rb
@@ -1,35 +1,26 @@
-require 'pathname'
-require 'tempfile'
+require File.dirname(__FILE__) + "/common"
 
-here = Pathname(__FILE__).dirname
-require here + "common"
-root = here + ".."
-readme = root + "README.rdoc"
-lib = root + "lib"
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../devel"
+require "jumpstart"
 
-describe readme do
-  $LOAD_PATH.unshift root + "devel"
-  require "jumpstart/ruby"
-  ["Synopsis"].each { |section|
-    describe section do
-      it "should run as claimed" do
-        contents = readme.read
-  
-        code = %{
-          $LOAD_PATH.unshift "#{lib.expand_path}"
-          require 'rubygems'
-        } + contents.match(%r!== #{section}.*?\n(.*?)^\S!m)[1]
-  
-        expected = code.scan(%r!\# => (.*?)\n!).flatten.join("\n")
-  
-        Tempfile.open("pure-readme") { |file|
-          file.puts code
-          file.close
-          result = `"#{Jumpstart::Ruby::EXECUTABLE}" "#{file.path}"`
-          raise unless $?.exitstatus
-          result.chomp.should == expected
-        }
-      end
+readme = "README.rdoc"
+
+Jumpstart.doc_to_spec(readme, "Synopsis")
+
+Jumpstart.doc_to_spec(readme, "Dynamic Example") { |expected, actual|
+  # check for numbers, not values
+  [actual, expected].each { |expr|
+    unless expr =~ %r!\A\d+\Z!
+      raise "readme failed"
     end
   }
-end
+  [nil, nil]
+}
+
+Jumpstart.doc_to_spec(readme, "Sexp Example") { |*expressions|
+  expressions.map { |expresssion|
+    result = eval(expresssion, TOPLEVEL_BINDING)
+    result[:add].merge!(:file => nil, :line => nil)
+    result
+  }
+}
